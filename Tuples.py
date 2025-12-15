a0685e55b93a69e76fc5539209082277464c2b21
Julio <juliomoreno7217@gmail.com>
2025-12-14 21:19:53 -0600

    Doc : Create "Tuples" file for practice

---
+++ Tuples.py
@@ -0,0 +1,12 @@
+techs = ("python", "javascript", "Go")
+print(techs)
+
+tuple_1 = [1,2,3]
+tuple_2 = [4,5,6]
+tuple_3 = tuple_1 + tuple_2
+print(tuple_3)
+
+print(tuple_3*2)
+
+for item in tuple_3:
+    print(item)
