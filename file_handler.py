8638b55f83e0571e3af7357fa04e1c0a96082194
Julio <juliomoreno7217@gmail.com>
2025-12-31 11:30:25 -0600

    Doc : Create "archivo and file handler" for practicing file creation and edition

---
+++ file_handler.py
@@ -0,0 +1,25 @@
+# Reading file content
+try:
+    with open("archivo.txt","r",encoding="utf-8") as f:
+        print(f.readline())
+        print(f.readline())
+except FileNotFoundError:
+    print("No se pudo abrir el archivo")
+
+# Modifying file content
+try:
+    with open("archivo.txt","w") as f:
+        f.write("File has been modified")
+    with open("archivo.txt","r",encoding="utf-8") as f:
+        print(f.readline())
+except FileNotFoundError:
+    print("No se pudo abrir el archivo")
+
+# Adding more content
+try:
+    with open("archivo.txt","a") as f:
+        f.write("File has been modified")
+    with open("archivo.txt","r",encoding="utf-8") as f:
+        print(f.read())
+except FileNotFoundError:
+    print("No se pudo abrir el archivo")
