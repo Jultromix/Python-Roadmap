bf1b430293245a7ab5b82222b4d6468f1c54b7ea
Julio <juliomoreno7217@gmail.com>
2026-01-24 12:39:48 -0600

    Feat : Add finally as part of error handling in try-except file.
    Feat : Creat customized errors

--- try-except.py
+++ try-except.py
@@ -1,21 +1,28 @@
-a = 0
-b = 0
-result = None
+# a = 0
+# b = 0
+# result = None
 
-try:
-    a = int(input("Add a number "))
-    b = int(input("Add a number "))
-except Exception as e:
-    print("What you typed is not a number")
-    print(f"error: {e} | error type: {type(e)}")
+# try:
+#     a = int(input("Add a number "))
+#     b = int(input("Add a number "))
+# except Exception as e:
+#     print("What you typed is not a number")
+#     print(f"error: {e} | error type: {type(e)}")
 
-try:
-    result = a / b
-except Exception as e:
-    print("Division by 0 not allowed")
-    print(f"error: {e} | error type: {type(e)}")
+# try:
+#     result = a / b
+# except Exception as e:
+#     print("Division by 0 not allowed")
+#     print(f"error: {e} | error type: {type(e)}")
 
-print(f"Result: {result}")
+# print(f"Result: {result}")
+
+
+class DivisionError(Exception):
+    """ "Error in operation"""
+
+    pass
+
 
 ### A cleaner and specific version of capturing exceptions should be
 a = 0
@@ -25,9 +32,15 @@ result = None
 try:
     a = int(input("Add a number "))
     b = int(input("Add a number "))
+    if b == 2:
+        raise DivisionError("Custom_Division_by_2_Error")
     result = a / b
     print(f"Result: {result}")
 except ValueError:
     print("What you typed is not a number")
 except ZeroDivisionError:
     print("It's not allowed to divide by 0")
+finally:
+    print("print from finally")
+
+print("print out of error handlers")
