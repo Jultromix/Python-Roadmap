c74ff3120b0ee7a3a74b98cd707a3e0ec5d2f83e
Julio <juliomoreno7217@gmail.com>
2026-02-16 21:02:08 -0600

    Feat: Add more properties and make several objects of the same instance, then print them using a for loop

--- OPP_Course/main.py
+++ OPP_Course/main.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello from opp-course!")
+class Book:
+    def __init__(self, title: str, author: str, isbn: str, availability: bool):
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+        self.availability = availability
 
 
-if __name__ == "__main__":
-    main()
+catalog = [
+    Book("The Book of Classes", "JM", "123456789", True),
+    Book("Cheesecake Recipes", "KV", "234567890", False),
+    Book("The Book of Books of Classes", "Dante Visabais", "345678901", True),
+    Book("Chemisrty II", "J.v Dallas", "345678901", False),
+]
+
+for index, book in enumerate(catalog, start=1):
+    print(
+        f"{index} - title: {book.title} | author: {book.author} | isbn: {book.isbn} | availability: {book.availability}"
+    )
