0549f731042cdfb0ae9dfc6e93df57336de67ebe
Julio <juliomoreno7217@gmail.com>
2026-03-29 09:36:03 -0600

    Feat_ Add time format

--- FastAPI_Course/main.py
+++ FastAPI_Course/main.py
@@ -26,3 +26,19 @@ async def time(iso_code: str):
         return {"error": "Country code not found"}
     tz = zoneinfo.ZoneInfo(timezone_str)
     return {"timestamp": datetime.now(tz)}
+
+
+@app.get("/time/{iso_code}/{format}")
+async def time_format(iso_code: str, format: str):
+    iso = iso_code.upper()
+    timezone_str = country_timezones.get(iso)
+    if timezone_str is None:
+        return {"error": "Country code not found"}
+    tz = zoneinfo.ZoneInfo(timezone_str)
+
+    if format == "12":
+        return {"timestamp": datetime.now(tz).strftime("%I:%M:%S %p")}
+    elif format == "24":
+        return {"timestamp": datetime.now(tz).strftime("%H:%M:%S")}
+    else:
+        return {"timestamp": datetime.now(tz)}
