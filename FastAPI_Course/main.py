0035a483b7477218ad6194932c986de380a5b5d8
Julio <juliomoreno7217@gmail.com>
2026-03-29 09:29:06 -0600

    Feat: Add timezone dynamic end point

--- FastAPI_Course/main.py
+++ FastAPI_Course/main.py
@@ -1,4 +1,6 @@
+import zoneinfo
 from fastapi import FastAPI
+from datetime import datetime
 
 app = FastAPI()
 
@@ -6,3 +8,21 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+country_timezones = {
+    "MX": "America/Mexico_City",
+    "CO": "America/Bogota",
+    "BR": "America/Sao_Paulo",
+    "PE": "America/Lima",
+}
+
+
+@app.get("/time/{iso_code}")
+async def time(iso_code: str):
+    iso = iso_code.upper()
+    timezone_str = country_timezones.get(iso)
+    if timezone_str is None:
+        return {"error": "Country code not found"}
+    tz = zoneinfo.ZoneInfo(timezone_str)
+    return {"timestamp": datetime.now(tz)}
