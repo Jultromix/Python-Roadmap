46de5794e7cee34f55a9ec377dc8d9691fa7b949
Julio <juliomoreno7217@gmail.com>
2026-04-12 19:58:03 -0600

    Doc: Create unit test file for transactions

--- FastAPI_Course/app/tests/tests_transaction.py
+++ FastAPI_Course/app/tests/tests_transaction.py
@@ -2,5 +2,11 @@ from fastapi import status
 
 
 def test_create_transaction(client):
-    response = client.post("/transaction", json={"amount": 100})
+    response = client.post(
+        "/transaction",
+        json={
+            "amount": 100,
+        },
+    )
+    print("response = " + str(response.status_code))
     assert response.status_code == status.HTTP_201_CREATED
