ef789735b5afb598260ca4a363032a376def28eb
Julio <juliomoreno7217@gmail.com>
2026-05-31 18:54:05 -0600

    Fix: Query performance content was corrected, previous code sessions was inserted

--- Databases Core/Query_Performance.sql
+++ Databases Core/Query_Performance.sql
@@ -1,80 +1,46 @@
--- Show the growth of sales per coutnry from one month to the other
+-- Show top 10 queries which consumed the most from the CPU since the las reboot
+SELECT TOP 10
+	SUBSTRING(st.text, (qs.statement_start_offset/2) + 1,
+	((CASE qs.statement_end_offset WHEN -1 THEN DATALENGTH(st.text)
+	ELSE qs.statement_end_offset END - qs.statement_start_offset)/2) + 1) AS QueryText,
+	qs.execution_count,
+	qs.total_worker_time / 1000							AS TotalCPU_ms,
+	qs.total_worker_time / qs.execution_count / 1000	AS PromedioCPU_ms,
+	qs.last_logical_reads / qs.execution_count			AS Promedio_readings,
+	qs.last_elapsed_time / qs.execution_count / 1000	AS Promedio_tiempo_ms
+
+FROM sys.dm_exec_query_stats qs
+CROSS APPLY sys.dm_exec_sql_text(qs.sql_handle) st
+ORDER BY qs.total_worker_time DESC;
+
+-- The top 10 most cost consuming reads
+SELECT TOP 10
+	SUBSTRING(st.text, (qs.statement_start_offset/2) + 1,
+	((CASE qs.statement_end_offset WHEN -1 THEN DATALENGTH(st.text)
+	ELSE qs.statement_end_offset END - qs.statement_start_offset)/2) + 1) AS QueryText,
+	qs.execution_count,
+	qs.total_elapsed_time / qs.execution_count / 1000	AS Promedio_tiempo_ms,
+	qs.last_logical_reads / qs.execution_count			AS Promedio_readings
+FROM sys.dm_exec_query_stats qs
+CROSS APPLY sys.dm_exec_sql_text(qs.sql_handle) st
+ORDER BY qs.total_logical_reads DESC;
+
+
+-- Which are the indexes with less usage
 SELECT
-	pa.NombrePais,
-	YEAR(p.FechaPedido)		AS Anio,
-	MONTH(p.FechaPedido)	AS Mes,
-	SUM(p.Total)			AS MonthlySales,
-
-	LAG(SUM(p.Total)) OVER(
-		PARTITION BY pa.PaisID ORDER BY YEAR(p.FechaPedido), MONTH(p.FechaPedido)
-	) AS PreviousMonthSales,
-
-	SUM(p.Total) - 
-	LAG(SUM(p.Total)) OVER(
-		PARTITION BY pa.PaisID ORDER BY YEAR(p.FechaPedido), MONTH(p.FechaPedido)
-	) AS AbsoluteDelta,
-
-	CAST(
-		(SUM(p.Total) - 
-		LAG(SUM(p.Total)) OVER(
-			PARTITION BY pa.PaisID ORDER BY YEAR(p.FechaPedido), MONTH(p.FechaPedido)
-		)) * 100 /
-
-		NULLIF(		--(avoids division by 0)
-			LAG(SUM(p.Total)) OVER(PARTITION BY pa.PaisID ORDER BY YEAR(p.FechaPedido), MONTH(p.FechaPedido)),
-			0
-		) AS DECIMAL(10,2)
-	)	 AS PercentualGrowth
-FROM Pedidos p
-INNER JOIN Clientes c ON p.ClienteID = c.ClienteID
-INNER JOIN Paises pa ON c.PaisID = pa.PaisID
-WHERE p.Estado = 'Completado'
-GROUP BY pa.PaisID, pa.NombrePais, YEAR(p.FechaPedido), MONTH(p.FechaPedido)
-ORDER BY pa.NombrePais, Anio, Mes
-
-
-
--- What is the accumulated value of sales per country and it's percentage against the total of sales?
-SELECT
-	pa.NombrePais,
-	SUM(p.Total)			AS SalesPerCountry,
-
-	SUM(SUM(p.Total)) OVER() AS TotalGlobal,
-	SUM(SUM(p.Total)) OVER(ORDER BY SUM(p.Total) DESC ROWS UNBOUNDED PRECEDING) AS RankingAcc,
-
-	CAST(
-		SUM(p.Total) * 100 /
-		SUM(SUM(p.Total)) OVER () AS DECIMAL(7,2) 
-	) AS PercentageFromTotal
-FROM Pedidos p
-INNER JOIN Clientes c ON p.ClienteID = c.ClienteID
-INNER JOIN Paises pa ON c.PaisID = pa.PaisID
-WHERE p.Estado = 'Completado'
-GROUP BY pa.PaisID, pa.NombrePais
-ORDER BY SalesPerCountry
-
--- Rank Top 5 months with highest sales For Argentina
-WITH RankMonthlySales AS (
-	SELECT
-		pa.NombrePais,
-		YEAR(p.FechaPedido)		AS Anio,
-		MONTH(p.FechaPedido)	AS Mes,
-		SUM(p.Total)			AS MonthlySales,
-		RANK() OVER(PARTITION BY pa.PaisID ORDER BY YEAR(p.FechaPedido),MONTH(p.FechaPedido) DESC)	AS Ranking
-	FROM Pedidos p
-	INNER JOIN Clientes c ON p.ClienteID = c.ClienteID
-	INNER JOIN Paises pa ON c.PaisID  = pa.PaisID
-	WHERE p.Estado = 'Completado'
-
-	GROUP BY pa.PaisID, pa.NombrePais,YEAR(p.FechaPedido), MONTH(p.FechaPedido)
-)
-
-SELECT TOP 5
-	NombrePais,
-	Anio,
-	Mes,
-	MonthlySales,
-	Ranking
-FROM RankMonthlySales
-WHERE NombrePais = 'Argentina'
-ORDER BY Ranking
+	OBJECT_NAME(i.object_id)	AS Tabla,
+	i.name						AS NombreIndice,
+	i.type_desc					AS Tipo,
+	ius.user_seeks				AS Seeks,
+	ius.user_lookups			AS Lookups,
+	ius.user_updates			AS Actualizaciones
+FROM sys.indexes i
+LEFT JOIN sys.dm_db_index_usage_stats ius
+	ON i.object_id = ius.object_id
+	AND i.index_id = ius.index_id
+	AND ius.database_id = DB_ID()
+WHERE OBJECT_NAME(i.object_id)	IS NOT NULL
+	AND i.type_desc <> 'HEAP'
+	AND (ius.user_seeks) = 0 OR ius.user_seeks IS NULL
+	AND (ius.user_scans) = 0 OR ius.user_scans IS NULL
+ORDER BY ius.user_updates DESC;
