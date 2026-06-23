2f996547042d7817f76b843c49d163b3129f5496
Julio <juliomoreno7217@gmail.com>
2026-06-23 01:37:03 -0600

    Feat: Adding more functions and their respective test functions.

--- Unit_Testing/tests/tests_calculator.py
+++ Unit_Testing/tests/tests_calculator.py
@@ -1,10 +1,21 @@
 import unittest
-from src.calculator import addition, subtraction
+
+from src.calculator import addition, division, multiplication, subtraction
 
 
 class CalculatorTests(unittest.TestCase):
     def test_addition(self):
         assert addition(1, 2) == 3
 
-    def test_substraction(self):
+    def test_subtraction(self):
         assert subtraction(100, 50) == 50
+
+    def test_multiplication(self):
+        assert multiplication(5, 4) == 20
+
+    def test_division(self):
+        assert division(10, 2) == 5
+
+    def test_division_by_zero(self):
+        with self.assertRaises(ValueError):
+            division(10, 0)
