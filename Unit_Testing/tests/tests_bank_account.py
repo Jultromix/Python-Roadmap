826685f9e3bd17234013e38fc573630755124942
Julio <juliomoreno7217@gmail.com>
2026-06-28 11:16:33 -0600

    Feat: Creating Setup function to inicialize the account with a constant balance and avoid declaring every single time for each test

--- Unit_Testing/tests/tests_bank_account.py
+++ Unit_Testing/tests/tests_bank_account.py
@@ -3,16 +3,17 @@ from src.bank_account import BankAccount
 
 
 class BankAccountTests(unittest.TestCase):
+    def setUp(self):
+        self.account = BankAccount(balance=1000)
+
     def test_deposit(self):
-        account = BankAccount()
-        assert account.deposit(55) == 55
+        new_balance = self.account.deposit(55)
+        assert new_balance == 1055
 
     def test_withdraw(self):
-        account = BankAccount(balance=1000)
-        new_balance = account.withdraw(500)
+        new_balance = self.account.withdraw(500)
         assert new_balance == 500
 
     def test_get_balance(self):
-        account = BankAccount(balance=1000)
-        balance = account.get_balance()
+        balance = self.account.get_balance()
         assert balance == 1000
