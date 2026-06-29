5728f00920ee33ee716c7290b2dfeed4a13bdb0e
Julio <juliomoreno7217@gmail.com>
2026-06-28 18:42:47 -0600

    Fix: refactor the types of assertions based on the need of the test and the data being used.

--- Unit_Testing/tests/tests_bank_account.py
+++ Unit_Testing/tests/tests_bank_account.py
@@ -17,21 +17,21 @@ class BankAccountTests(unittest.TestCase):
 
     def test_deposit(self):
         new_balance = self.account.deposit(55)
-        assert new_balance == 1055
+        self.assertEqual(new_balance, 1055, "the balance is not equal")
 
     def test_withdraw(self):
         new_balance = self.account.withdraw(500)
-        assert new_balance == 500
+        self.assertEqual(new_balance, 500, "the balance is not equal")
 
     def test_get_balance(self):
         balance = self.account.get_balance()
-        assert balance == 1000
+        self.assertEqual(balance, 1000, "the balance is not equal")
 
     def test_transfer(self):
         target_account = BankAccount(balance=500)
         new_balance = self.account.transfer(200, target_account)
-        assert new_balance == 800
-        assert target_account.get_balance() == 700
+        self.assertEqual(new_balance, 800, "the balance is not equal")
+        self.assertEqual(target_account.get_balance(), 700, "the balance is not equal")
         with self.assertRaises(ValueError):
             self.account.transfer(2000, target_account)  # Insufficient funds
 
@@ -40,17 +40,15 @@ class BankAccountTests(unittest.TestCase):
         self.assertTrue(os.path.exists(self.account.log_file))
 
     def test_count_transactions(self):
-        assert self._count_lines(self.account.log_file) == 1
+        self.assertEqual(self._count_lines(self.account.log_file), 1)
 
         self.account.deposit(100)
-        assert (
-            self._count_lines(self.account.log_file) == 2
-        )  # Account creation and deposit
+        self.assertEqual(self._count_lines(self.account.log_file), 2)
 
         # Verifying the log for transfers
         target_account = BankAccount(balance=500)
         self.account.transfer(200, target_account)
-        assert target_account.get_balance() == 700
+        self.assertEqual(target_account.get_balance(), 700)
         assert self._count_lines(self.account.log_file) == 3
 
         with self.assertRaises(ValueError):
