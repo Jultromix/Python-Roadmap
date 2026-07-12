ee95994412118b9a9f021502babd2599ab934067
Julio <juliomoreno7217@gmail.com>
2026-07-11 22:34:51 -0600

    Feat: Use side_efect to use a set of values and test the behavior of the application against them.

--- Unit_Testing/tests/test_api_client.py
+++ Unit_Testing/tests/test_api_client.py
@@ -1,6 +1,7 @@
 import unittest
 from src.api_client import get_location, get_rates
 from unittest.mock import patch
+import requests
 
 
 class ApiClientTests(unittest.TestCase):
@@ -29,3 +30,44 @@ class ApiClientTests(unittest.TestCase):
             "Country code should be US",
         )
         mock_get.assert_called_once_with("https://free.freeipapi.com/api/json/8.8.8.8")
+
+    @patch("src.api_client.requests.get")
+    def test_get_location_returns_side_effect(self, mock_get):
+        mock_get.side_effect = [
+            requests.exceptions.RequestException("Service Unavailable"),
+            unittest.mock.Mock(
+                status_code=200,
+                json=lambda: {
+                    "countryName": "USA",
+                    "cityName": "Mountain View",
+                    "continent": "North America",
+                    "countryCode": "US",
+                },
+            ),
+        ]
+        # mock_get.return_value.status_code = 200       #These are part of the side effect
+        # mock_get.return_value.json.return_value = {
+        #     "countryName": "USA",
+        #     "cityName": "Mountain View",
+        #     "continent": "North America",
+        #     "countryCode": "US",
+        # }
+
+        with self.assertRaises(requests.exceptions.RequestException):
+            get_location("8.8.8.8")
+
+        result = get_location("8.8.8.8")
+        self.assertEqual(result.get("country"), "USA", "Country should be USA")
+        self.assertEqual(
+            result.get("city"), "Mountain View", "City should be Mountain View"
+        )
+        self.assertEqual(
+            result.get("continent"),
+            "North America",
+            "Continent should be North America",
+        )
+        self.assertEqual(
+            result.get("countrycode"),
+            "US",
+            "Country code should be US",
+        )
